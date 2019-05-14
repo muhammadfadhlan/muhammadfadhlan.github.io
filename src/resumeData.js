dc5e81c83fc60df6e35ee151a44458c2b2518f4b
muhammadfadhlan <43231290+muhammadfadhlan@users.noreply.github.com>
2019-05-14 13:30:42 +0700

    upload
    a

---
+++ src/resumeData.js
@@ -0,0 +1,102 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+
+    "socialLinks":[
+        {
+          "name":"youtube",
+          "url":"https://youtube.com/ifadworld/",
+          "className":"fa fa-youtube"
+        },
+        {
+          "name":"soundcloud",
+          "url":"http://soundcloud.com/ifadworld",
+          "className":"fa fa-music"
+        },
+        {
+          "name":"spinnin",
+          "url":"https://spinninrecords.com/profile/ifad-world",
+          "className":"fa fa-play"
+        },
+        {
+          "name":"spinnin",
+          "url":"https://www.deviantart.com/ifadworld33",
+          "className":"fa fa-camera"
+        }
+      ],
+    "aboutme":"Full Name : Muhammad Fadhlan Ahfazh.       I am currently a student at Gadjah Mada University, Yogyakarta, Indonesia",
+    "address":"fadhlan33@gmail.com",
+    "website":"https://ifadworld.cf",
+    "education":[
+      {
+        "UniversityName":"Universitas Gadjah Mada",
+        "specialization":"Vocational College",
+        "YearOfPassing":"2017-Present",
+        "Achievements":"D3 : Computer & System Information"
+      },
+      {
+        "UniversityName":"SMAN 1 Cirebon",
+        "specialization":"Science",
+        "YearOfPassing":"2014-2017",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Music Mashup",
+        "specialization":"Indiependent Label;Fimro & IFADWORLD Limited;Spinnin Records",
+        "YearOfLeaving":"2010-Present",
+        "Achievements":"IFADWORLD Mashup is my project to synchronize any songs into different music style, or usually in a form of a song, created by blending two or more pre-recorded songs, usually by overlaying the vocal track of one song seamlessly over the instrumental track of another. This project has been started since 2010. None of this projects are commercially used."
+      },
+      {
+        "CompanyName":"Music Performer",
+        "specialization":"FREEZE",
+        "YearOfLeaving":"2010-2013",
+        "Achievements":"Drummer/Performer"
+      }
+    ],
+    "skillsDescription":"Here what I do",
+    "skills":[
+      {
+        "skillname":"Music Re-Composing"
+      },
+      {
+        "skillname":"Videography & Photography"
+      },
+      {
+        "skillname":"Web Programming"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"Photography Project",
+        "imgurl":"images/portfolio/take_me_higher_by_ifadworld33_dcfxdp3.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"Videography Project",
+        "imgurl":"images/portfolio/vlcsnap-error939.png"
+      },
+      {
+        "name":"project3",
+        "description":"Music Project",  
+        "imgurl":"images/portfolio/ifadworldstudio.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"Visual Project",
+        "imgurl":"images/portfolio/20150803_1652312.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Wow bro voted, insane track so far. You got a very good mastering and nice Synth´s here. I hope you get what you earn for that hard work. Good luck in the contest :) ",
+        "name":"Annihilation"
+      },
+      {
+        "description":"I really liked your track. Voted. Your style is magnificent. I would like to record a collab with you.",
+        "name":"Dmitrii Stucalschi"
+      }
+    ]
+  }
+  
+  export default resumeData
